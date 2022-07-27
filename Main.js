6745d3c4f3021b91150e6c6b729c4d4b583f1127
curtiende <109224646+curtiende@users.noreply.github.com>
2022-07-27 20:57:05 +0300

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function(){
+    return(
+        <div className='main'>
+             <h3 className='facts-title'>Fan Facts about React</h3>
+             <ul className= 'main-facts'>
+                <li>Was first released in 2013</li>
+                <li>Was orinially created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
---
+++ Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar(){
+    return(
+        <div className='navbar'>
+            <div className='nav-left'>
+                <img width='60px' src='./images/react-logo.png'/>
+                <h3>ReactFacts</h3>
+            </div>
+            <h3>React Course - Project 1</h3>
+        </div>)
+}
