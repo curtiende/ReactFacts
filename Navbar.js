760df8923af7417e617a5c1ac4b526de9290dc83
curtiende <109224646+curtiende@users.noreply.github.com>
2022-07-27 20:58:37 +0300

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar(){
     return(
         <div className='navbar'>
             <div className='nav-left'>
-                <img width='60px' src='./images/react-logo.png'/>
+                <img width='60px' src='./react-logo.png'/>
                 <h3>ReactFacts</h3>
             </div>
             <h3>React Course - Project 1</h3>
