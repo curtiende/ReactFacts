4311bfbb1ae5400ee5f46d6bf187ebbc5bec1636
curtiende <109224646+curtiende@users.noreply.github.com>
2022-07-27 20:52:05 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Main from './components/Main'
+import Navbar from './components/Navbar'
+
+export default function App(){
+    return(
+        <div>
+            <Navbar/>
+            <Main/>
+        </div>
+    )
+    
+}
